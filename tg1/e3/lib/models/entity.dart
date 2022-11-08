51a31f40a73e880813c42012bb296c1167e3aa20
Miguel Leirosa <miguelleirosa@gmail.com>
2022-11-08 12:22:00 +0000

    tg1:e3 - created entity class

---
+++ tg1/e3/lib/models/entity.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import '../util/generateid.dart';
+
+class Entity {
+  // Class instances cache data
+  static final _cache = <int, Entity>{};
+
+  // Variables
+  late String _name;
+  late int _age;
+  late String _address;
+
+  // Constructors
+  // It follows the Singleton Pattern by checking its cache for duplicates
+  factory Entity(String name, int age, String address) {
+    int id = generateID([name]);
+    return _cache.putIfAbsent(id, () => Entity._internal(name, age, address));
+  }
+  // Internal constructor (Isn't saved into cache)
+  Entity._internal(this._name, this._age, this._address);
+
+  // Getters
+  static Map<int, Entity> get cache => _cache;
+  String get name => _name;
+  int get age => _age;
+  String get address => _address;
+  int get id => generateID([_name]);
+
+  //Setters
+  // Verify if entity already exists
+  set name(e){
+    Entity tempObj = Entity._internal(e, age, address);
+    if(!_cache.containsKey(tempObj.id)){
+      _cache.remove(id);
+      _name = e;
+      _cache.putIfAbsent(id, () => this);
+    } else {
+      throw 'Already exists!';
+    }
+  }
+  set age(e) => _age = e;
+  set address(e) => _address = e;
+
+  // Return object in JSON format
+  String toJSON() {
+    Map json = {
+      'id' : id,
+      'name' : _name,
+      'age':  _age,
+      'address': _address
+    };
+    JsonEncoder encoder = new JsonEncoder.withIndent('  ');
+    return encoder.convert(json);
+  }
+
+  // Print cache data
+  static printCache(){
+    _cache.forEach((key, value) {
+      Map json = {
+        'key' : key,
+        'value':  {
+          'id' : value.id,
+          'name' : value.name,
+          'age':  value.age,
+          'address': value.address
+        }
+      };
+      JsonEncoder encoder = new JsonEncoder.withIndent('  ');
+      print(encoder.convert(json));
+    });
+  }
+}
