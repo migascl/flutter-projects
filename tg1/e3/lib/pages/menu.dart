e2f4d8e881314f1b6301c9067b4f9a4f801b9d83
Miguel Leirosa <miguelleirosa@gmail.com>
2022-11-11 12:08:24 +0000

    tg1:e3 - added main menu page

---
+++ tg1/e3/lib/pages/menu.dart
@@ -0,0 +1,29 @@
+// Imports
+import 'dart:io';
+import 'package:e3/pages/manager/manager.dart';
+import 'package:e3/pages/reports.dart';
+import 'dashboard.dart';
+
+void menu(){
+  dashboard();
+  stdout.writeln("Menu:");
+  stdout.writeln("\t1. Relatórios");
+  stdout.writeln("\t2. Dados");
+  stdout.write("Insira o digito da opção desejada: ");
+  try {
+    String? option = stdin.readLineSync();
+    switch(option) {
+      case '1':
+        reports();
+        break;
+      case '2':
+        manager();
+        break;
+      default:
+        stdout.writeln("Opção inválida!");
+    }
+  } catch (e) {
+    stdout.writeln("Opção inválida!");
+  }
+  menu(); // Recursion until correct option
+}
