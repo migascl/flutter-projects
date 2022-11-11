81f158f19740b03e17a90bf46a3a7073772b3f11
Miguel Leirosa <miguelleirosa@gmail.com>
2022-11-11 11:13:12 +0000

    tg1:e3 - completed remove function

--- tg1/e3/lib/pages/manager/list.dart
+++ tg1/e3/lib/pages/manager/list.dart
@@ -1,4 +1,4 @@
-// Libary imports
+// Library imports
 import 'dart:io';
 import 'package:e3/util/globals.dart';
 import '../../models/entity.dart';
@@ -8,6 +8,7 @@ import '../../models/policy.dart';
 // Menu to view all data
 // Requires an object type to determine which data type to list.
 void list(Type obj) {
+  stdout.writeln("");
   switch(obj) {
     case Entity:
       stdout.writeln("Entidades:");
@@ -19,6 +20,28 @@ void list(Type obj) {
         stdout.writeln("\t└ Idade: ${entity.age}");
         stdout.writeln("\t└ Morada: ${entity.address}");
       }
+      stdout.write("Digite o número do elemento da lista para selecionar uma entidade:");
+      try {
+        String? index = stdin.readLineSync();
+        Entity entity = Entity.cache.entries.elementAt(int.parse(index!)).value;
+        stdout.write("Editar (e) ou Remover (r) ${entity.name} (${entity.id}): ");
+        String? option = stdin.readLineSync();
+        switch(option) {
+          case 'e':
+
+            break;
+          case 'r':
+            Entity.remove(entity);
+            stdout.writeln("Entidade apagada.");
+            break;
+          default:
+            stdout.writeln("Opção inválida!");
+        }
+      } on DependencyException {
+        stdout.writeln("Existem apólices associadas a esta entidade.");
+      } catch (e) {
+        stdout.writeln("Opção inválida! $e");
+      }
       break;
     case Insurer:
       stdout.writeln("Seguradoras");
@@ -27,6 +50,28 @@ void list(Type obj) {
       for(int i = 0; i < insurers.length; i++){
         Insurer insurer = insurers.entries.elementAt(i).value;
         stdout.writeln("$i. ${insurer.name} (${insurer.id})");
+      }
+      stdout.write("Digite o número do elemento da lista para selecionar uma seguradora:");
+      try {
+        String? index = stdin.readLineSync();
+        Insurer insurer = Insurer.cache.entries.elementAt(int.parse(index!)).value;
+        stdout.write("Editar (e) ou Remover (r) ${insurer.name} (${insurer.id}): ");
+        String? option = stdin.readLineSync();
+        switch(option) {
+          case 'e':
+
+            break;
+          case 'r':
+            Insurer.remove(insurer);
+            stdout.writeln("Seguradora apagada.");
+            break;
+          default:
+            stdout.writeln("Opção inválida!");
+        }
+      } on DependencyException {
+        stdout.writeln("Existem apólices associadas a esta seguradora.");
+      } catch (e) {
+        stdout.writeln("Opção inválida! $e");
       }
       break;
     case Policy:
@@ -45,6 +90,29 @@ void list(Type obj) {
         stdout.writeln("\t└ Valor Prémio: ${policy.billingCost} "
             "${(policy.insuranceType == BillingTypes.monthly) ? ("por mês") : ("por ano") }");
         stdout.writeln("\t└ Estado: ${(policy.active) ? "Ativo" : "Inativo"}");
+      }
+      stdout.write("Digite o número do elemento da lista para selecionar uma apólice:");
+      try {
+        String? index = stdin.readLineSync();
+        Policy policy = Policy.cache.entries.elementAt(int.parse(index!)).value;
+        stdout.write("Editar (e) ou Remover (r) "
+            "${insuranceTypeToString(policy.insuranceType)} "
+            "${policy.insurer.name} "
+            "(${policy.id}): ");
+        String? option = stdin.readLineSync();
+        switch(option) {
+          case 'e':
+
+            break;
+          case 'r':
+            Policy.remove(policy);
+            stdout.writeln("Apólice apagada.");
+            break;
+          default:
+            stdout.writeln("Opção inválida!");
+        }
+      } catch (e) {
+        stdout.writeln("Opção inválida! $e");
       }
       break;
   }
