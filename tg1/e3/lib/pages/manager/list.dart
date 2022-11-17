4695b7ba7de49b3939ec150bfb5f911fff53dda9
Miguel Leirosa <miguelleirosa@gmail.com>
2022-11-17 12:18:19 +0000

    tg1:e3 - added policy edit function

--- tg1/e3/lib/pages/manager/list.dart
+++ tg1/e3/lib/pages/manager/list.dart
@@ -130,7 +130,84 @@ void list(Type type) {
         String? option = stdin.readLineSync();
         switch(option) {
           case 'e':
-
+            // Select Insurer
+            stdout.writeln("\tSeguradora: ");
+            Map<int, Insurer> insurers = Insurer.cache;
+            for(int i = 0; i < insurers.length; i++){
+              Insurer insurer = insurers.entries.elementAt(i).value;
+              stdout.writeln("\t\t$i. ${insurer.name} (${insurer.id})");
+            }
+            stdout.write("\tDigite o número do elemento da lista para selecionar a seguradora: ");
+            String? insurerIndex = stdin.readLineSync();
+            if(insurerIndex!.isNotEmpty) policy.insurer = Insurer.cache.entries.elementAt(int.parse(insurerIndex)).value;
+            // Select Entities
+            Map<int, Entity> entities = Entity.cache;
+            // Select holder Entity
+            stdout.writeln("\tTomador: ");
+            for(int i = 0; i < entities.length; i++){
+              Entity entity = entities.entries.elementAt(i).value;
+              stdout.writeln("\t\t$i. ${entity.name} (${entity.id})");
+            }
+            stdout.write("\tDigite o número do elemento da lista para selecionar o tomador: ");
+            String? holderIndex = stdin.readLineSync();
+            if(holderIndex!.isNotEmpty) policy.holder = Entity.cache.entries.elementAt(int.parse(holderIndex)).value;
+            // Select insured Entity
+            stdout.writeln("\tSegurado: ");
+            for(int i = 0; i < entities.length; i++){
+              Entity entity = entities.entries.elementAt(i).value;
+              stdout.writeln("\t\t$i. ${entity.name} (${entity.id})");
+            }
+            stdout.write("\tDigite o número do elemento da lista para selecionar o tomador: ");
+            String? insuredIndex = stdin.readLineSync();
+            if(insuredIndex!.isNotEmpty) policy.insured = Entity.cache.entries.elementAt(int.parse(insuredIndex)).value;
+            // Select Insurance Type
+            stdout.writeln("\tTipo de Seguro: ");
+            for(int i = 0; i < InsuranceTypes.values.length; i++){
+              InsuranceTypes insuranceType = InsuranceTypes.values.elementAt(i);
+              stdout.writeln("\t\t$i. ${insuranceTypeToString(insuranceType)}");
+            }
+            stdout.write("\tDigite o número do elemento da lista para selecionar o tipo de seguro: ");
+            String? insuranceTypeIndex = stdin.readLineSync();
+            if(insuranceTypeIndex!.isNotEmpty) policy.insuranceType = InsuranceTypes.values.elementAt(int.parse(insuranceTypeIndex));
+            // Insured Amount
+            stdout.write("\tValor Segurado: ");
+            String? insuredAmount = stdin.readLineSync();
+            if(insuredAmount!.isNotEmpty) policy.insuredAmount = double.parse(insuredAmount);
+            // Select Billing Type
+            stdout.writeln("\tTipo de Prémio: ");
+            for(int i = 0; i < BillingTypes.values.length; i++){
+              stdout.writeln("\t\t$i. "
+                  "${(BillingTypes.values.elementAt(i) == BillingTypes.monthly)
+                  ? ("Mensal")
+                  : ("Anual") }");
+            }
+            stdout.write("\tDigite o número do elemento da lista para selecionar o tipo de prémio: ");
+            String? billingTypeIndex = stdin.readLineSync();// Billing Cost
+            if(billingTypeIndex!.isNotEmpty) policy.billingType = BillingTypes.values.elementAt(int.parse(billingTypeIndex));
+            // Billing Cost
+            stdout.write("\tValor do Prémio a pagar: ");
+            String? billingCost = stdin.readLineSync();
+            if(billingCost!.isNotEmpty) policy.billingCost = double.parse(billingCost);
+            // Select state
+            stdout.writeln("\tEstado: ");
+            stdout.writeln("\t\ta. Ativo: ");
+            stdout.writeln("\t\tb. Inativo: ");
+            stdout.write("\tDigite o caráter correspondente ao estado da apólice: ");
+            String? stateIndex = stdin.readLineSync();
+            bool? active;
+            switch(stateIndex){
+              case 'a':
+                active = true;
+                break;
+              case 'b':
+                active = false;
+                break;
+            }
+            if(active != null) policy.active = active;
+            stdout.writeln("Apólice "
+                "${insuranceTypeToString(policy.insuranceType)} "
+                "${policy.insurer.name} "
+                "(${policy.id}) alterada.");
             break;
           // Remove Policy
           case 'r':
