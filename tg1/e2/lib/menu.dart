dcf3f98668e252e97a89900ff71aff5b92722b0b
Miguel Leirosa <miguelleirosa@gmail.com>
2022-11-21 13:10:03 +0000

    tg1:e2 - update menu

--- tg1/e2/lib/menu.dart
+++ tg1/e2/lib/menu.dart
@@ -4,38 +4,38 @@ import 'list.dart';
 
 // Menu console
 void menu(list) {
-  print("----------------------------------------------------");
-  print("Conjunto: $list");
-  print("Menu:");
-  print("\t1. Maior e menor elemento do conjunto.");
-  print("\t2. Amplitude do conjunto.");
-  print("\t3. Listagem dos números impares em ordem decrescente.");
-  print("Escreva o número da opção desejada: ");
+  stdout.writeln("\n-----------------------------------------------------------------------------------------------------------");
+  stdout.writeln("Conjunto: $list");
+  stdout.writeln("Menu:");
+  stdout.writeln("\t1. Maior e menor elemento do conjunto.");
+  stdout.writeln("\t2. Amplitude do conjunto.");
+  stdout.writeln("\t3. Listagem dos números impares em ordem decrescente.");
+  stdout.write("Escreva o número da opção desejada: ");
   // Receives input from the user, and catches any type error
   try {
     int? option = int.parse(stdin.readLineSync()!);
     switch(option) {
       case 1:
-        print("\nMaior e menor elemento:");
+        stdout.writeln("\nMaior e menor elemento:");
         Map<String, int> result = getMinMax(list);
-        print("\tMenor: ${result['min']}");
-        print("\tMaior: ${result['max']}");
+        stdout.writeln("\tMenor: ${result['min']}");
+        stdout.writeln("\tMaior: ${result['max']}");
         break;
       case 2:
-        print("\nAmplitude do conjunto: ${getRange(list)}");
+        stdout.writeln("\nAmplitude do conjunto: ${getRange(list)}");
         break;
       case 3:
-        print("\nLista dos números ímpares em ordem decrescente:");
+        stdout.writeln("\nLista dos números ímpares em ordem decrescente:");
         print(getOdds(list));
         break;
       default:
-        print("Opção inválida!");
+        stdout.writeln("Opção inválida!");
     }
   } catch (e) {
-    print("Opção inválida!");
+    stdout.writeln("Opção inválida!");
   }
   // User prompt to go back to menu
-  print("\nPrima Enter para voltar ao menu.");
+  stdout.write("\nPrima Enter para voltar ao menu.");
   stdin.readLineSync();
   menu(list);
 }
