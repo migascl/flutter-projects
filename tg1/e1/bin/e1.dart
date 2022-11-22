d317b02770be18674f7ec1cc80b3647cb9b7cf3a
Miguel Leirosa <miguelleirosa@gmail.com>
2022-11-22 11:57:55 +0000

    tg1:e1 - merge code into main

--- tg1/e1/bin/e1.dart
+++ tg1/e1/bin/e1.dart
@@ -1,6 +1,19 @@
-import 'package:e1/e1.dart';
-
 void main(List<String> arguments) {
+  // Variables
+  int start = 100;
+  int finish = 200;
+
   // Initialize the calculator with start and finish intervals
-  calculate(100, 200);
+  print('Intervalo: ]$start, $finish[');
+  // Loop that runs through the entire interval checking if current number (i) its a prime number and add it to sum
+  var sum = 0;
+  for (var i = start; i <= finish; i++) {
+    if (i % 2 == 1) {
+      sum += i;
+    }
+  }
+  print('Soma: $sum');
+  // Calculate average of prime numbers by dividing the sum with the number of elements of the interval
+  var avg = sum / (finish - start);
+  print('Média: $avg');
 }
