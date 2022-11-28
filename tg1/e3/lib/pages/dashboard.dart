9dc530ca156a3dafd66a3c0a2eff44de2bbc77b6
Miguel Leirosa <miguelleirosa@gmail.com>
2022-11-28 15:16:15 +0000

    tg1:e3 - update dashboard

--- tg1/e3/lib/pages/dashboard.dart
+++ tg1/e3/lib/pages/dashboard.dart
@@ -6,48 +6,61 @@ import '../models/policy.dart';
 
 // Displays summed up version of the data
 void dashboard() {
-  stdout.writeln("\nQuantidade de Apólices:");
-  // Query for total active insurance policies
-  int totalActivePolicies = 0;
-  Policy.cache.forEach((key, value) {
-    if (value.active) totalActivePolicies++;
-  });
-  stdout.writeln("\t├ Ativas: $totalActivePolicies");
-  stdout.writeln("\t└ Inativas: ${Policy.cache.length - totalActivePolicies}");
-
-
-  // Query for active policies based on insurance type
-  stdout.writeln("\tPor Categoria:");
-  InsuranceTypes.values.forEach((type) {
-    int _activePolicies = 0;
-    double _sumActivePolicies = 0;
-    Policy.cache.entries.toList().where((e) => e.value.active && e.value.insuranceType == type)
-        .forEach((policy) {
-          _activePolicies++;
-          _sumActivePolicies += policy.value.insuredAmount;
+  if(Policy.cache.isNotEmpty) {
+    stdout.writeln("\nQuantidade de Apólices:");
+    // Query for total active insurance policies
+    int totalActivePolicies = 0; // Number of active policies
+    Policy.cache.forEach((key, value) {
+      if (value.active) totalActivePolicies++;
     });
-    double _avgActivePolicies = _sumActivePolicies / _activePolicies;
-    if(_activePolicies > 0){
-      stdout.writeln("\t\t- ${type.name}: $_activePolicies");
-      stdout.writeln("\t\t  └ Média: $_avgActivePolicies€");
-    }
-  });
+    stdout.writeln("\t├ Ativas: $totalActivePolicies");
+    stdout.writeln(
+        "\t└ Inativas: ${Policy.cache.length - totalActivePolicies}");
 
 
-  // Query for active insurance policies based on insurer
-  stdout.writeln("\tPor Seguradora:");
-  Insurer.cache.forEach((key, value) {
-    int _activePolicies = 0;
-    double _sumActivePolicies = 0;
-    Policy.cache.entries.toList().where((e) => e.value.active && e.value.insurer == value)
-        .forEach((element) {
-      _activePolicies++;
-      _sumActivePolicies += element.value.insuredAmount;
+    // Query for active policies based on insurance type
+    stdout.writeln("\tPor Categoria:");
+    InsuranceTypes.values.forEach((type) {
+      int _activePolicies = 0; // Number of active policies
+      double _sumInsuredAmount = 0; // Sum of all policies insured amount
+      // Query
+      Policy.cache.entries.toList().where((e) =>
+      e.value.active && e.value.insuranceType == type)
+          .forEach((policy) {
+        _activePolicies++;
+        _sumInsuredAmount += policy.value.insuredAmount;
+      });
+      double _avgInsuredAmounts = _sumInsuredAmount /
+          _activePolicies; // Calculate average insured amount
+      // Only print result if found any policies
+      if (_activePolicies > 0) {
+        stdout.writeln("\t\t- ${type.name}: $_activePolicies");
+        stdout.writeln("\t\t  └ Média: $_avgInsuredAmounts€");
+      }
     });
-    double _avgActivePolicies = _sumActivePolicies / _activePolicies;
-    if(_activePolicies > 0) {
-      stdout.writeln("\t\t- ${value.name}: $_activePolicies");
-      stdout.writeln("\t\t  └ Média: $_avgActivePolicies€");
-    }
-  });
+
+
+    // Query for active insurance policies based on insurer
+    stdout.writeln("\tPor Seguradora:");
+    Insurer.cache.forEach((key, value) {
+      int _activePolicies = 0; // Number of active policies
+      double _sumInsuredAmount = 0; // Sum of all policies insured amount
+      // Query
+      Policy.cache.entries.toList().where((e) =>
+      e.value.active && e.value.insurer == value)
+          .forEach((element) {
+        _activePolicies++;
+        _sumInsuredAmount += element.value.insuredAmount;
+      });
+      double _avgInsuredAmounts = _sumInsuredAmount /
+          _activePolicies; // Calculate average insured amount
+      // Only print result if found any policies
+      if (_activePolicies > 0) {
+        stdout.writeln("\t\t- ${value.name}: $_activePolicies");
+        stdout.writeln("\t\t  └ Média: $_avgInsuredAmounts€");
+      }
+    });
+  } else {
+    stdout.writeln("\nNão existem apólices.");
+  }
 }
