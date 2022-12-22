81a58bb7b4f3159be980522bd0693be38fa91754
Miguel Leirosa <miguelleirosa@gmail.com>
2022-12-22 10:38:45 +0000

    tg2 - merge startup to main

--- tg2/app/lib/main.dart
+++ tg2/app/lib/main.dart
@@ -1,14 +1,17 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:tg2/pages/start_page.dart';
+import 'package:http/http.dart' as http;
+import 'package:tg2/utils/constants.dart';
+import 'package:tg2/views/screens/home_view.dart';
 
 void main() {
-  runApp(const TG2());
+  runApp(const Main());
 }
 
-class TG2 extends StatelessWidget {
-  const TG2({super.key});
+class Main extends StatelessWidget {
+  const Main({super.key});
 
-  // This widget is the root of your application.
+  // Root of the application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +19,77 @@ class TG2 extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const StartPage(apiUri: "http://10.0.2.2:3000"),
+      home: StartUpView(),
     );
+  }
+}
+
+// Screen to verify if app has connection to API
+class StartUpView extends StatefulWidget {
+  @override
+  State<StartUpView> createState() => _StartUpView();
+}
+
+class _StartUpView extends State<StartUpView> {
+  // This is used to test the connection to the API during launch
+  // If connection fails, an alert dialog will show and give the possibility of trying again
+  Future _getConnection() async {
+    late var result; // Response or error code variable
+
+    try {
+      var response = await http
+          .get(
+              Uri.parse('${apiUrl}/')) // Get response from root endpoint of API
+          .timeout(const Duration(
+              seconds:
+                  5)); // Throw Timeout Exception if takes longer than 5 seconds
+      result = response.statusCode;
+    } on TimeoutException {
+      result = TimeoutException;
+    } catch (e) {
+      result = e;
+    }
+
+    // If response is successful (code 200) navigate to Home page, show error dialog if not
+    if (result == 200) {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => HomeView(),
+          maintainState: false,
+        ),
+      );
+    } else {
+      showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+                title: const Text('Ocorreu um erro!'),
+                content: Text(
+                    'Não foi possível estabelecer conexão ao servidor.\nErro $result.'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      _getConnection();
+                    },
+                    child: const Text('Tentar Novamente'),
+                  ),
+                ],
+              ));
+    }
+  }
+
+  // Attempt connection on initial state
+  @override
+  void initState() {
+    super.initState();
+    _getConnection();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: CircularProgressIndicator(),
+    ));
   }
 }
