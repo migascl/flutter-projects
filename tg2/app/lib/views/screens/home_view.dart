a435ca115c14f58f48d0f21117008b483c949f4c
Miguel Leirosa <miguelleirosa@gmail.com>
2022-12-27 16:50:55 +0000

    tg2 - update home view

--- tg2/app/lib/views/screens/home_view.dart
+++ tg2/app/lib/views/screens/home_view.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:tg2/provider/club_provider.dart';
+import 'package:tg2/provider/country_provider.dart';
+import 'package:tg2/provider/player_provider.dart';
+import 'package:tg2/provider/stadium_provider.dart';
+import '../../models/club_model.dart';
+import '../../models/country_model.dart';
+import '../../models/player_model.dart';
+import '../../models/stadium_model.dart';
+import 'club/club_view.dart';
+
+enum Sections { matchweeks, clubs, players }
 
 class HomeView extends StatefulWidget {
   @override
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
+  late TabController _tabController;
+  Sections currentSection = Sections.clubs;
+
   @override
   void initState() {
     super.initState();
@@ -13,29 +27,102 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    AppBar appBar;
+    Widget body;
+
+    switch (currentSection) {
+      case Sections.matchweeks:
+        _tabController = TabController(length: 3, vsync: this);
+        appBar = AppBar(
+          title: Text("Jornadas"),
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: const <Widget>[
+              Tab(
+                icon: Icon(Icons.cloud_outlined),
+              ),
+              Tab(
+                icon: Icon(Icons.beach_access_sharp),
+              ),
+              Tab(
+                icon: Icon(Icons.brightness_5_sharp),
+              ),
+            ],
+          ),
+        );
+        body = TabBarView(
+          controller: _tabController,
+          children: const <Widget>[
+            Center(
+              child: Text("It's cloudy here"),
+            ),
+            Center(
+              child: Text("It's rainy here"),
+            ),
+            Center(
+              child: Text("It's sunny here"),
+            ),
+          ],
+        );
+        break;
+      case Sections.clubs:
+        appBar = AppBar(title: Text("Clubes"));
+        body = ClubListView();
+        break;
+      case Sections.players:
+        appBar = AppBar(title: Text("Jogadores"));
+        body = PlayerListView();
+        break;
+    }
+
     return Scaffold(
-        body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-            onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => MatchweeksView(),
-                    maintainState: false,
-                  ),
-                ),
-            child: Text("Jornadas")),
-        ElevatedButton(
-            onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => TeamsView(),
-                    maintainState: false,
-                  ),
-                ),
-            child: Text("Clubes")),
-      ],
-    )));
+        appBar: appBar,
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.blue),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text("Liga Portugal bwin"),
+                      Text("Época 2021-2022")
+                    ],
+                  )),
+              ListTile(
+                leading: Icon(Icons.calendar_month_rounded),
+                title: Text("Jornada"),
+                onTap: () {
+                  setState(() {
+                    currentSection = Sections.matchweeks;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.group),
+                title: Text("Clubes"),
+                onTap: () {
+                  setState(() {
+                    currentSection = Sections.clubs;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.directions_run_rounded),
+                title: Text("Jogadores"),
+                onTap: () {
+                  setState(() {
+                    currentSection = Sections.players;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: body);
   }
 }
 
@@ -101,19 +188,135 @@ class _MatchweeksViewState extends State<MatchweeksView> {
   }
 }
 
-class TeamsView extends StatefulWidget {
-  const TeamsView({super.key});
+class ClubListView extends StatefulWidget {
+  const ClubListView({super.key});
 
   @override
-  State<TeamsView> createState() => _TeamsViewState();
+  State<ClubListView> createState() => _ClubListViewState();
 }
 
-class _TeamsViewState extends State<TeamsView> {
+class _ClubListViewState extends State<ClubListView> {
+  late Future<Map<int, Club>> _getAllClubs;
+
+  @override
+  void initState() {
+    super.initState();
+    _getAllClubs = ClubProvider().getAll();
+  }
+
   @override
   Widget build(BuildContext context) {
-    print("Teams View: Building...");
-    return Scaffold(
-        appBar: AppBar(title: Text("Clubes")),
-        body: Center(child: Text("Equipas")));
+    return FutureBuilder<Map<int, Club>>(
+        future: _getAllClubs,
+        builder: (context, AsyncSnapshot<Map<int, Club>> snapshot) {
+          if (snapshot.hasData) {
+            List<Club> list = snapshot.data!.values.toList();
+            return ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                Club club = list.elementAt(index);
+                return FutureBuilder<Stadium>(
+                    future: StadiumProvider().getByID(club.id),
+                    builder: (context, AsyncSnapshot<Stadium> snapshot) {
+                      if (snapshot.hasData) {
+                        Stadium stadium = snapshot.data!;
+                        return Column(
+                          children: [
+                            ListTile(
+                              leading: Image(
+                                image: NetworkImage(club.icon),
+                                height: 32,
+                              ),
+                              title: Text(club.name),
+                              subtitle: Text(stadium.name),
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => ClubView(id: club.id),
+                                    maintainState: true,
+                                  ),
+                                );
+                              },
+                            ),
+                            const Divider(
+                              height: 2.0,
+                            ),
+                          ],
+                        );
+                      } else {
+                        return Container();
+                      }
+                    });
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
+}
+
+class PlayerListView extends StatefulWidget {
+  const PlayerListView({super.key});
+
+  @override
+  State<PlayerListView> createState() => _PlayerListViewState();
+}
+
+class _PlayerListViewState extends State<PlayerListView> {
+  late Future<Map<int, Player>> _getAllPlayers;
+
+  @override
+  void initState() {
+    super.initState();
+    _getAllPlayers = PlayerProvider.getAll();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Map<int, Player>>(
+        future: _getAllPlayers,
+        builder: (context, AsyncSnapshot<Map<int, Player>> snapshot) {
+          if (snapshot.hasData) {
+            List<Player> list = snapshot.data!.values.toList();
+            return ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                Player player = list.elementAt(index);
+                return FutureBuilder<Country>(
+                    future: CountryProvider().getByID(player.countryID),
+                    builder: (context, AsyncSnapshot<Country> snapshot) {
+                      if (snapshot.hasData) {
+                        Country country = snapshot.data!;
+                        return Column(
+                          children: [
+                            ListTile(
+                              title: Text(player.name),
+                              subtitle: Text(country.name),
+                              onTap: () {
+                                // TODO NAVIGATION TO PLAYER VIEW
+                              },
+                            ),
+                            const Divider(
+                              height: 2.0,
+                            ),
+                          ],
+                        );
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    });
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
   }
 }
