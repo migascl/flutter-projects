8c050f6eba0fa0de95411a553a207de41f9310d4
Miguel Leirosa <miguelleirosa@gmail.com>
2022-12-29 14:05:22 +0000

    tg2 - update club provider

--- tg2/app/lib/provider/club_provider.dart
+++ tg2/app/lib/provider/club_provider.dart
@@ -1,50 +1,39 @@
 // Library imports
 import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:tg2/utils/constants.dart';
-import 'package:tg2/utils/exceptions.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:tg2/models/club_model.dart';
+import 'package:tg2/utils/api/api_endpoints.dart';
+import 'package:tg2/utils/api/api_service.dart';
+import 'package:tg2/utils/constants.dart';
 
 // Club provider class
-class ClubProvider {
-  // Methods
-  Future<Club> getByID(int id) async {
-    try {
-      print("CLUB: Fetching $id...");
-      var response = await http
-          .get(Uri.parse("${apiUrl}/club?id=eq.${id}"))
-          .timeout(const Duration(seconds: 5));
-      if (response.statusCode == 200) {
-        List<dynamic> json = jsonDecode(response.body);
-        if (json.isEmpty) throw ResponseException;
-        return Club.fromJson(json.first as Map<String, dynamic>);
-      } else {
-        throw ResponseException;
-      }
-    } catch (e) {
-      print(e);
-      rethrow;
-    }
+class ClubProvider extends ChangeNotifier {
+  Map<int, Club> _items = {};
+  ProviderState _state = ProviderState.empty;
+
+  // Automatically fetch data when initialized
+  ClubProvider() {
+    print("Club/P: Initialized");
+    get();
   }
 
-  Future<Map<int, Club>> getAll() async {
+  ProviderState get state => _state;
+  Map<int, Club> get items => _items;
+
+  Future<void> get() async {
     try {
-      print("CLUB: Fetching all...");
-      var response = await http
-          .get(Uri.parse("${apiUrl}/club"))
-          .timeout(const Duration(seconds: 5));
-      late List<dynamic> json;
-      (response.statusCode == 200)
-          ? json = jsonDecode(response.body)
-          : throw ResponseException;
-      json = jsonDecode(response.body);
-      if (json.isEmpty) throw ResponseException;
-      return Map<int, Club>.fromIterable(json.toList(),
-          key: (item) => item['id'], value: (item) => Club.fromJson(item));
+      print("Club/P: Getting all...");
+      _state = ProviderState.busy;
+      notifyListeners();
+      final response = await ApiService().get(ApiEndpoints.club);
+      _items = { for (var item in response) item['id'] : Club.fromJson(item) };
+      print("Club/P: Fetched successfully!");
+      _state = ProviderState.ready;
+      notifyListeners();
     } catch (e) {
-      print(e);
-      rethrow;
+      print("Club/P: Error fetching! $e");
+      _state = ProviderState.empty;
+
     }
   }
 }
--- tg2/app/lib/utils/api/api_endpoints.dart
+++ tg2/app/lib/utils/api/api_endpoints.dart
@@ -2,7 +2,8 @@
 enum ApiEndpoints {
   root(endpoint: '/'),
   country(endpoint: '/country'),
-  stadium(endpoint: '/stadium');
+  stadium(endpoint: '/stadium'),
+  club(endpoint: '/club');
 
   final String endpoint;
 
