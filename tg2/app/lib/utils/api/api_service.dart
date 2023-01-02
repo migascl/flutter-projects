5c2e2420e77b4a93d70f67afe1027de9bd852c62
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-02 16:07:43 +0000

    tg2 - added api delete service

--- tg2/app/lib/utils/api/api_service.dart
+++ tg2/app/lib/utils/api/api_service.dart
@@ -9,7 +9,7 @@ class ApiService {
   static const String _apiUrl = "http://10.0.2.2:3000"; // Api url
 
   // Api GET method, it recieves an endpoint and fetches all results
-  // It throws errors if the response timesout, status code is not valid or response body is empty
+  // It throws errors if the response times out, status code is not valid or response body is empty
   Future<dynamic> get(ApiEndpoints endpoint) async {
     var response = await http
         .get(Uri.parse(_apiUrl + endpoint.endpoint))
@@ -19,6 +19,20 @@ class ApiService {
       var decodedJson = jsonDecode(response.body);
       if(decodedJson.isEmpty) throw EmptyDataException();
       return decodedJson;
+    } else {
+      throw ResponseException("http Response returned status code ${response.statusCode}");
+    }
+  }
+
+  // Api DELETE method, it recieves an endpoint and id of the element
+  // It throws errors if the response times out or status code is not valid
+  Future<dynamic> delete(ApiEndpoints endpoint, int id) async {
+    var response = await http
+        .delete(Uri.parse(_apiUrl + endpoint.endpoint + "?id=eq." + id.toString()))
+        .timeout(const Duration(seconds: 5));
+    await Future.delayed(Duration(milliseconds: 200));
+    if (response.statusCode == 200) {
+      return true;
     } else {
       throw ResponseException("http Response returned status code ${response.statusCode}");
     }
