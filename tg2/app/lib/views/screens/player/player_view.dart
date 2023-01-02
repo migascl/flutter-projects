33305be69046a837a06f7bac0441a8bbae6d1a9e
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-02 11:35:07 +0000

    tg2 - add player info & exams page

--- tg2/app/lib/views/screens/player/player_view.dart
+++ tg2/app/lib/views/screens/player/player_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:tg2/provider/stadium_provider.dart';
-import '../../../models/club_model.dart';
+import 'package:provider/provider.dart';
+import 'package:tg2/utils/constants.dart';
+import '../../../models/exam_model.dart';
 import '../../../models/player_model.dart';
-import '../../../models/stadium_model.dart';
-import '../../../provider/club_provider.dart';
+import '../../../provider/country_provider.dart';
+import '../../../provider/exam_provider.dart';
 
 class PlayerView extends StatefulWidget {
   const PlayerView({super.key, required this.player});
@@ -48,24 +49,30 @@ class _PlayerViewState extends State<PlayerView> {
                 icon: Icon(Icons.file_copy_rounded), label: 'Contratos')
           ],
           onTap: _onTappedBar,
-          selectedItemColor: Colors.orange,
           currentIndex: _selectedIndex,
         ),
+        // TODO IMPROVE PLAYER HEADER STYLE
         body: Column(children: [
           Container(
               color: Colors.blue,
               height: 200,
               padding: EdgeInsets.fromLTRB(16, 86, 16, 16),
               alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  Image(
+                    height: 64,
+                    image: NetworkImage(widget.player.picture),
+                  ),
                   Text(
-                    widget.player.name,
+                    widget.player.nickname ?? widget.player.name,
                     style: TextStyle(
                         fontFamily: 'Roboto',
                         fontWeight: FontWeight.normal,
-                        fontSize: 24)
+                        fontSize: 24,
+                        color: Colors.white,
+                    )
                   )
                 ],
               )),
@@ -73,8 +80,67 @@ class _PlayerViewState extends State<PlayerView> {
               child: PageView(
                 controller: _pageController,
                 children: [
-                  Center(child: Text("${widget.player.height}")),
-                  Center(child: Text("Exames")),
+                  ListView(
+                    padding: const EdgeInsets.all(8),
+                    children: [
+                      ListTile(
+                        title: Text("Nome"),
+                        subtitle: Text(widget.player.name)
+                      ),
+                      ListTile(
+                        title: Text("Nacionalidade"),
+                        subtitle: Text(Provider.of<CountryProvider>(context, listen: false).items[widget.player.countryID]!.name),
+                      ),
+                      ListTile(
+                        title: Text("Data de Nascimento"),
+                        subtitle: Text("${widget.player.birthday} (${widget.player.age} anos)"),
+                      ),
+                      ListTile(
+                        title: Text("Altura"),
+                        subtitle: Text("${widget.player.height} cm"),
+                      ),
+                      ListTile(
+                        title: Text("Peso"),
+                        subtitle: Text("${widget.player.weight} kg"),
+                      ),
+                    ],
+                  ),
+                  Consumer<ExamProvider>(builder: (context, examProvider, child) {
+                    if(examProvider.state == ProviderState.ready) {
+                      Map<int, Exam> examList = Map.fromEntries(examProvider.items.entries.expand((element) => [
+                        if (element.value.playerID == widget.player.id) MapEntry(element.key, element.value)
+                      ]));
+                      if(examList.isEmpty) {
+                        return const Center(child: Text("Nenhum exam encontrado."));
+                      }
+                      return ListView.builder(
+                        itemCount: examList.length,
+                        itemBuilder: (context, index) {
+                          Exam exam = examList.values.elementAt(index);
+                          return Column(
+                            children: [
+                              ListTile(
+                                title: Text("Exame #${exam.id}"),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("Data: ${exam.date}"),
+                                    Text("Resultado: ${(exam.result) ? "Passou" : "Falhou"}")
+                                  ],
+                                ),
+                              ),
+                              const Divider(
+                                height: 2.0,
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      return const Center(child: CircularProgressIndicator(),);
+                    }
+                  }),
+                  // TODO ADD PLAYER CONTRACTS VIEW
                   Center(child: Text("Contratos")),
                 ],
                 onPageChanged: (page) {
