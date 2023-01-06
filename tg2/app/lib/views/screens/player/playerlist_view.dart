3cd7b16210e1cfffaaf846c9830ddef1c4934f61
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-06 16:01:45 +0000

    tg2 - update player list view

--- tg2/app/lib/views/screens/player/playerlist_view.dart
+++ tg2/app/lib/views/screens/player/playerlist_view.dart
@@ -1,12 +1,11 @@
-// Library Imports
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tg2/provider/exam_provider.dart';
 import 'package:tg2/provider/player_provider.dart';
 import 'package:tg2/views/screens/player/player_view.dart';
 import 'package:tg2/models/country_model.dart';
 import 'package:tg2/models/player_model.dart';
 import 'package:tg2/utils/constants.dart';
-
 
 // This page lists all players
 class PlayerListView extends StatefulWidget {
@@ -17,6 +16,23 @@ class PlayerListView extends StatefulWidget {
 }
 
 class _PlayerListViewState extends State<PlayerListView> {
+  final GlobalKey<RefreshIndicatorState> _playerListRefreshKey =
+      GlobalKey<RefreshIndicatorState>();
+
+  // Method to reload providers used by the page
+  Future _loadPageData() async {
+    try {
+      await Provider.of<PlayerProvider>(context, listen: false).get();
+      await Provider.of<ExamProvider>(context, listen: false).get();
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Ocorreu um erro. Tente novamente.'),
+          duration: Duration(seconds: 5),
+        ),
+      );
+    }
+  }
 
   @override
   void initState() {
@@ -28,61 +44,51 @@ class _PlayerListViewState extends State<PlayerListView> {
   Widget build(BuildContext context) {
     print("PlayerList/V: Building...");
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Jogadores"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh_rounded),
-            tooltip: 'Refresh',
-            onPressed: () {
-              // Refresh page data
-              Provider.of<PlayerProvider>(context, listen: false).get();
-            },
-          ),
-        ],
-      ),
-      // TODO ADD CREATE FUNCTION
-      floatingActionButton: null,
-      body: Consumer<PlayerProvider>(builder: (context, playerProvider, child) {
-        // Wait until provider is ready
-        if(playerProvider.state == ProviderState.ready) {
-            Map<int, Player> playerList = playerProvider.items; // Player list
-            Map<int, Country> countryList = playerProvider.countryProvider.items; // Country list
-            return ListView.builder(
-              itemCount: playerList.length,
-              itemBuilder: (context, index) {
-                Player player = playerList.values.elementAt(index); // Get player by ID
-                Country country = countryList[player.countryID]!; // Get player's country by ID
-                return Column(
-                  children: [
-                    ListTile(
-                      leading: Image(
-                        image: NetworkImage(player.picture),
-                        height: 32,
-                      ),
-                      title: Text(player.name),
-                      subtitle: Text(country.name),
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (BuildContext context) => PlayerView(player: player),
-                            maintainState: false,
-                          ),
-                        );
-                      },
-                      onLongPress: () {
-                        // TODO ADD REMOVE FUNCTION
-                      },
-                    ),
-                    const Divider(height: 2.0)
-                  ],
-                );
-              },
-            );
-        } else {
-          return const Center(child: CircularProgressIndicator(),);
-        }
-      }),
-    );
+        appBar: AppBar(
+          title: const Text("Jogadores"),
+        ),
+        body: RefreshIndicator(
+          key: _playerListRefreshKey,
+          onRefresh: _loadPageData,
+          child: Consumer<PlayerProvider>(
+              builder: (context, playerProvider, child) {
+            // Wait until provider is ready
+            if (playerProvider.state == ProviderState.ready) {
+              return ListView.builder(
+                itemCount: playerProvider.items.length,
+                itemBuilder: (context, index) {
+                  Player player = playerProvider.items.values.elementAt(index);
+                  return Column(
+                    children: [
+                      ListTile(
+                          leading: (player.picture != null)
+                              ? Image(
+                                  image: NetworkImage(player.picture!),
+                                  height: 32,
+                                )
+                              : null,
+                          title: Text(player.name),
+                          subtitle: Text(player.country.name),
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    PlayerView(player: player),
+                                maintainState: false,
+                              ),
+                            );
+                          }),
+                      const Divider(height: 2.0)
+                    ],
+                  );
+                },
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+        ));
   }
 }
