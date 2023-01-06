5ce989a5de5fbf2ab0dd2fff28a7dfc04a986002
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-06 16:02:25 +0000

    tg2 - update match list view

--- tg2/app/lib/views/screens/match/matchlist_view.dart
+++ tg2/app/lib/views/screens/match/matchlist_view.dart
@@ -4,7 +4,6 @@ import 'package:tg2/provider/club_provider.dart';
 import 'package:tg2/provider/match_provider.dart';
 import 'package:tg2/models/match_model.dart';
 import 'package:tg2/utils/constants.dart';
-
 import '../../widgets/matchtile.dart';
 
 // This page lists all matches
@@ -15,14 +14,12 @@ class MatchListView extends StatefulWidget {
   State<MatchListView> createState() => _MatchListViewState();
 }
 
-class _MatchListViewState extends State<MatchListView>  {
-
-  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
+class _MatchListViewState extends State<MatchListView> {
   int _currentTab = 0;
 
   // Method to reload providers used by the page
   Future _loadPageData() async {
-    try{
+    try {
       await Provider.of<ClubProvider>(context, listen: false).get();
       await Provider.of<MatchProvider>(context, listen: false).get();
     } catch (e) {
@@ -44,58 +41,59 @@ class _MatchListViewState extends State<MatchListView>  {
   @override
   Widget build(BuildContext context) {
     print("MatchListView/V: Building...");
-    return Consumer2<ClubProvider, MatchProvider>(builder: (context, clubProvider, matchProvider, child) {
-      if(matchProvider.state == ProviderState.ready && clubProvider.state == ProviderState.ready) {
-        List<Widget> tabs = List.from(matchProvider.getMatchweeks().map((element) => Tab(text: "Jornada $element")));
-        return DefaultTabController(
-            initialIndex: _currentTab,
-            length: tabs.length,
-            child: Scaffold(
-            appBar: AppBar(
-              title: const Text('Clubes'),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.refresh_rounded),
-                  tooltip: 'Refresh',
-                  onPressed: () {
-                    _loadPageData();
-                  },
+    return Consumer2<ClubProvider, MatchProvider>(
+      builder: (context, clubProvider, matchProvider, child) {
+        if (matchProvider.state == ProviderState.ready &&
+            clubProvider.state == ProviderState.ready) {
+          List<Widget> tabs = List.from(matchProvider
+              .getMatchweeks()
+              .map((element) => Tab(text: "Jornada $element")));
+          return DefaultTabController(
+              initialIndex: _currentTab,
+              length: tabs.length,
+              child: Scaffold(
+                appBar: AppBar(
+                  title: const Text('Jornadas'),
+                  actions: [
+                    IconButton(
+                        icon: const Icon(Icons.refresh_rounded),
+                        tooltip: 'Refresh',
+                        onPressed: () => _loadPageData()),
+                  ],
+                  bottom: TabBar(
+                    tabs: tabs,
+                    onTap: (index) {
+                      setState(() {
+                        _currentTab = index;
+                      });
+                    },
+                  ),
                 ),
-              ],
-              bottom: TabBar(
-                  tabs: tabs,
-                  onTap: (index) {
-                    setState(() {
-                      _currentTab = index;
-                    });
-                  },
-              ),
-            ),
-            body: TabBarView(
-                children: List.from(matchProvider.getMatchweeks().map((element) {
-                  List<Match> matchweeks = matchProvider.items.values.where((match) => match.matchweek == element).toList();
-                  return Container(child:
-                      ListView.builder(
-                      itemCount: matchweeks.length,
-                      itemBuilder: (context, index) {
-                        Match match = matchweeks[index];
-                        return Column(
-                          children: [
-                            MatchTile(match: match),
-                            const Divider(height: 2.0),
-                          ],
-                        );
-                      },
-                      ),
+                body: TabBarView(
+                    children:
+                        List.from(matchProvider.getMatchweeks().map((element) {
+                  List<Match> matchweeks = matchProvider.items.values
+                      .where((match) => match.matchweek == element)
+                      .toList();
+                  return ListView.builder(
+                    itemCount: matchweeks.length,
+                    itemBuilder: (context, index) {
+                      Match match = matchweeks[index];
+                      return Column(
+                        children: [
+                          MatchTile(match: match),
+                          const Divider(height: 2.0),
+                        ],
+                      );
+                    },
                   );
-                }
-                ))
-              ),
-        ));
-      }
-      return Scaffold(appBar: AppBar(title: Text("Clubes")),
-      );
-    },
+                }))),
+              ));
+        }
+        return Scaffold(
+          appBar: AppBar(title: Text("Clubes")),
+        );
+      },
     );
   }
 }
