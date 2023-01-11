eaff893b4aecf519710bc7b314bfab61930e1998
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-11 15:34:54 +0000

    tg2 - add contract activity flag

--- tg2/app/lib/models/contract_model.dart
+++ tg2/app/lib/models/contract_model.dart
@@ -17,13 +17,24 @@ class Contract {
   final int? _id; // Only used when getting from JSON
 
   // Constructor
-  Contract(this.player, this.club, this.number, this.position, this.period, this.document, [this._id]);
+  Contract(this.player, this.club, this.number, this.position, this.period,
+      this.document,
+      [this._id]);
 
   // Getters
   int? get id => _id;
+  bool get active {
+    DateTime date = DateTime.now();
+    return date.isAfter(period.start) && date.isBefore(period.end)
+        ? true
+        : false;
+  }
+
   // Calculate if contract will need renovating in the next 6 months
   bool get needsRenovation {
     DateTime date = DateTime.now();
-    return DateTime(date.year, date.month + 6, date.day).isAfter(period.end)  ? true : false;
+    return DateTime(date.year, date.month + 6, date.day).isAfter(period.end)
+        ? true
+        : false;
   }
 }
