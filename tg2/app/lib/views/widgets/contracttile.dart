71e7b32624b1df5183a7f2d29bff7fe52fa7e382
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-25 17:12:52 +0000

    tg2 - update contract tile trailings

--- tg2/app/lib/views/widgets/contracttile.dart
+++ tg2/app/lib/views/widgets/contracttile.dart
@@ -6,7 +6,7 @@ import 'package:tg2/utils/dateutils.dart';
 // Club's squad player tile. It displays basic player information from a contract.
 // It can display either the club or player depending on the flag its given
 class ContractTile extends StatelessWidget {
-  const ContractTile({
+  ContractTile({
     super.key,
     required this.contract,
     required this.showClub,
@@ -21,8 +21,34 @@ class ContractTile extends StatelessWidget {
   final bool showAlert; // Flag to show contract alerts (i.e. expiration)
   final VoidCallback? onTap; // Function   @override
 
+  Widget? _trailing;
+
   @override
   Widget build(BuildContext context) {
+    if (showAlert) {
+      if (!contract.active) {
+        _trailing = Tooltip(
+          message: 'Expirado à ${DateTime.now().difference(contract.period.end).inDays} dias!',
+          textAlign: TextAlign.center,
+          child: const Icon(
+            Icons.cancel_rounded,
+            size: 32,
+            color: Colors.red,
+          ),
+        );
+      } else if (contract.needsRenovation) {
+        _trailing = Tooltip(
+          message:
+              'Contrato expira em ${contract.remainingTime.inDays} dias!\n(${DateUtilities().toYMD(contract.period.end)})',
+          textAlign: TextAlign.center,
+          child: const Icon(
+            Icons.warning_rounded,
+            size: 32,
+            color: Colors.amber,
+          ),
+        );
+      }
+    }
     return ListTile(
       dense: dense,
       leading: FutureImage(
@@ -36,25 +62,13 @@ class ContractTile extends StatelessWidget {
       ),
       title: (showClub)
           ? Text(contract.club.name)
-          : Text(
-              "${contract.number}. ${contract.player.nickname ?? contract.player.name}"),
+          : Text("${contract.number}. ${contract.player.nickname ?? contract.player.name}"),
       subtitle: (showClub)
           ? Text(
               "Ínicio: ${DateUtilities().toYMD(contract.period.start)} | Fim: ${DateUtilities().toYMD(contract.period.end)}")
           : Text(contract.position.name),
       // Show warning icon that shows a tooltip with remaining contract duration and expiry date if contract is active
-      trailing: (contract.needsRenovation && showAlert && contract.active)
-          ? Tooltip(
-              message:
-                  'Contrato expira em ${contract.remainingTime.inDays} dias!\n(${DateUtilities().toYMD(contract.period.end)})',
-              textAlign: TextAlign.center,
-              child: const Icon(
-                Icons.warning_rounded,
-                size: 32,
-                color: Colors.amber,
-              ),
-            )
-          : null,
+      trailing: _trailing,
       onTap: () => onTap?.call(),
     );
   }
