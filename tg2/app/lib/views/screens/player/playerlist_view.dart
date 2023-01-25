c3a382d146917fd424d0a0796fe0a2f5c1b7915f
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-25 12:24:45 +0000

    tg2 - update player list view

--- tg2/app/lib/views/screens/player/playerlist_view.dart
+++ tg2/app/lib/views/screens/player/playerlist_view.dart
@@ -5,19 +5,20 @@ import 'package:tg2/provider/player_provider.dart';
 import 'package:tg2/utils/constants.dart';
 import 'package:tg2/views/screens/player/player_view.dart';
 import 'package:tg2/views/widgets/futureimage.dart';
+import 'package:tg2/models/exam_model.dart';
+import 'package:tg2/models/player_model.dart';
+import 'package:tg2/utils/dateutils.dart';
+import 'package:tg2/views/widgets/menudrawer.dart';
 
-import '../../../models/exam_model.dart';
-import '../../../models/player_model.dart';
-import '../../../utils/dateutils.dart';
-import '../../widgets/menudrawer.dart';
-
+// Enumerator of all possible filter states
 enum _ExamFilters {
   empty,
   hasTests,
   noTests,
 }
 
-// This page lists all players
+// This widget lists all players and allows to filter based on their exam records
+// (if they have any and if so, in what specified period of time)
 class PlayerListView extends StatefulWidget {
   const PlayerListView({super.key});
 
@@ -26,18 +27,22 @@ class PlayerListView extends StatefulWidget {
 }
 
 class _PlayerListViewState extends State<PlayerListView> {
-  final GlobalKey<RefreshIndicatorState> _playerListRefreshKey =
-      GlobalKey<RefreshIndicatorState>();
+  // Glocal key for refresh indicator
+  final GlobalKey<RefreshIndicatorState> _playerListRefreshKey = GlobalKey<RefreshIndicatorState>();
 
+  // Text field controllers
   TextEditingController dateStartFieldController = TextEditingController();
   TextEditingController dateEndFieldController = TextEditingController();
-  String? errorText;
+  String? errorText; // Date field error text (will activate if non-null)
 
-  _ExamFilters _filter = _ExamFilters.empty;
-  DateTimeRange _filterPeriod =
-      DateTimeRange(start: DateTime(1970, 1, 1), end: DateTime.now());
+  _ExamFilters _filter = _ExamFilters.empty; // Current Filter
+  // Exam period filter
+  DateTimeRange _filterPeriod = DateTimeRange(
+    start: DateTime(1970, 1, 1),
+    end: DateTime.now(),
+  );
 
-  // Method to reload providers used by the page
+  // Reload providers used by the page, displays snackbar if exception occurs
   Future _loadPageData() async {
     try {
       await Provider.of<PlayerProvider>(context, listen: false).get();
@@ -51,8 +56,7 @@ class _PlayerListViewState extends State<PlayerListView> {
     }
   }
 
-  void _setFilter(_ExamFilters newFilter) =>
-      setState(() => _filter = newFilter);
+  void _setFilter(_ExamFilters newFilter) => setState(() => _filter = newFilter);
 
   void _setPeriod(DateTimeRange? range) => setState(() {
         if (range != null) {
@@ -60,13 +64,13 @@ class _PlayerListViewState extends State<PlayerListView> {
           dateStartFieldController.text = DateUtilities().toYMD(range.start);
           dateEndFieldController.text = DateUtilities().toYMD(range.end);
         } else {
-          _filterPeriod =
-              DateTimeRange(start: DateTime(1970, 1, 1), end: DateTime.now());
+          _filterPeriod = DateTimeRange(start: DateTime(1970, 1, 1), end: DateTime.now());
           dateStartFieldController.clear();
           dateEndFieldController.clear();
         }
       });
 
+  // Method to show date picker modal and set filter period
   void _showDatePicker() async {
     final DateTimeRange? result = await showDateRangePicker(
       context: context,
@@ -87,129 +91,128 @@ class _PlayerListViewState extends State<PlayerListView> {
   Widget build(BuildContext context) {
     print("PlayerList/V: Building...");
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Jogadores"),
-          elevation: 1,
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.sort_rounded),
-              tooltip: 'Filtrar',
-              // TODO IMPROVE PLAYER LIST FILTER STYLING
-              onPressed: () => showDialog<String>(
-                context: context,
-                builder: (BuildContext context) {
-                  return Dialog(
-                    child: StatefulBuilder(
-                      builder: (context, setState) => Container(
-                        padding: const EdgeInsets.all(24),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
+      appBar: AppBar(
+        title: const Text("Jogadores"),
+        elevation: 1,
+        actions: [
+          // ############# Filter Popup #############
+          IconButton(
+            icon: const Icon(Icons.sort_rounded),
+            tooltip: 'Filtrar',
+            onPressed: () => showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => Dialog(
+                child: StatefulBuilder(
+                  builder: (context, setState) => Container(
+                    padding: const EdgeInsets.all(24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Filtros',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(0, 4, 0, 8),
+                          child: const Divider(thickness: 1),
+                        ),
+                        Text(
+                          'Realizou Testes?',
+                          style: Theme.of(context).textTheme.labelLarge,
+                        ),
+                        Wrap(
+                          spacing: 8,
                           children: [
-                            Text(
-                              'Filtros',
-                              style: Theme.of(context).textTheme.titleLarge,
+                            ActionChip(
+                              label: const Icon(Icons.highlight_remove_rounded),
+                              tooltip: "Remover Filtro",
+                              onPressed: () => setState(() => _setFilter(_ExamFilters.empty)),
                             ),
-                            Container(
-                              padding: const EdgeInsets.fromLTRB(0, 4, 0, 8),
-                              child: const Divider(thickness: 1),
+                            ChoiceChip(
+                              label: const Text('Sim'),
+                              selectedColor: Colors.blue,
+                              selected: _filter == _ExamFilters.hasTests,
+                              onSelected: (bool selected) =>
+                                  setState(() => _setFilter(_ExamFilters.hasTests)),
                             ),
-                            Text(
-                              'Realizou Testes?',
-                              style: Theme.of(context).textTheme.labelLarge,
+                            ChoiceChip(
+                              label: const Text('Não'),
+                              selectedColor: Colors.blue,
+                              selected: _filter == _ExamFilters.noTests,
+                              onSelected: (bool selected) =>
+                                  setState(() => _setFilter(_ExamFilters.noTests)),
                             ),
-                            Wrap(
-                              spacing: 8,
-                              children: [
-                                ActionChip(
-                                    label: const Icon(
-                                        Icons.highlight_remove_rounded),
-                                    tooltip: "Remover Filtro",
-                                    onPressed: () => setState(
-                                        () => _setFilter(_ExamFilters.empty))),
-                                ChoiceChip(
-                                    label: const Text('Sim'),
-                                    selectedColor: Colors.blue,
-                                    selected: _filter == _ExamFilters.hasTests,
-                                    onSelected: (bool selected) => setState(
-                                        () =>
-                                            _setFilter(_ExamFilters.hasTests))),
-                                ChoiceChip(
-                                    label: const Text('Não'),
-                                    selectedColor: Colors.blue,
-                                    selected: _filter == _ExamFilters.noTests,
-                                    onSelected: (bool selected) => setState(
-                                        () =>
-                                            _setFilter(_ExamFilters.noTests))),
-                              ],
-                            ),
-                            const SizedBox(height: 16),
-                            Text(
-                              'Período',
-                              style: Theme.of(context).textTheme.labelLarge,
-                            ),
-                            const SizedBox(height: 8),
-                            Row(
-                              children: [
-                                ActionChip(
-                                  label: const Icon(
-                                      Icons.highlight_remove_rounded),
-                                  tooltip: "Apagar data",
-                                  onPressed: () => _setPeriod(null),
-                                ),
-                                const SizedBox(width: 16),
-                                Flexible(
-                                  child: TextField(
-                                    controller: dateStartFieldController,
-                                    readOnly: true,
-                                    enabled: _filter != _ExamFilters.empty,
-                                    decoration: const InputDecoration(
-                                      labelText: "Início",
-                                      border: OutlineInputBorder(),
-                                    ),
-                                    onTap: () => _showDatePicker(),
-                                  ),
-                                ),
-                                const SizedBox(width: 8),
-                                Flexible(
-                                  child: TextField(
-                                    controller: dateEndFieldController,
-                                    readOnly: true,
-                                    enabled: _filter != _ExamFilters.empty,
-                                    decoration: const InputDecoration(
-                                      labelText: "Final",
-                                      border: OutlineInputBorder(),
-                                    ),
-                                    onTap: () => _showDatePicker(),
-                                  ),
-                                ),
-                              ],
-                            )
                           ],
                         ),
-                      ),
+                        const SizedBox(height: 16),
+                        Text(
+                          'Período',
+                          style: Theme.of(context).textTheme.labelLarge,
+                        ),
+                        const SizedBox(height: 8),
+                        Row(children: [
+                          ActionChip(
+                            label: const Icon(Icons.highlight_remove_rounded),
+                            tooltip: "Apagar data",
+                            onPressed: () => _setPeriod(null),
+                          ),
+                          const SizedBox(width: 16),
+                          Flexible(
+                            child: TextField(
+                              controller: dateStartFieldController,
+                              readOnly: true,
+                              enabled: _filter != _ExamFilters.empty,
+                              decoration: const InputDecoration(
+                                labelText: "Início",
+                                border: OutlineInputBorder(),
+                              ),
+                              onTap: () => _showDatePicker(),
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          Flexible(
+                            child: TextField(
+                              controller: dateEndFieldController,
+                              readOnly: true,
+                              enabled: _filter != _ExamFilters.empty,
+                              decoration: const InputDecoration(
+                                labelText: "Final",
+                                border: OutlineInputBorder(),
+                              ),
+                              onTap: () => _showDatePicker(),
+                            ),
+                          ),
+                        ])
+                      ],
                     ),
-                  );
-                },
+                  ),
+                ),
               ),
-            )
-          ],
-        ),
-        drawer: const MenuDrawer(),
-        body: RefreshIndicator(
-          key: _playerListRefreshKey,
-          onRefresh: _loadPageData,
-          child: Consumer2<PlayerProvider, ExamProvider>(
-              builder: (context, playerProvider, examProvider, child) {
+            ),
+          )
+        ],
+      ),
+      drawer: const MenuDrawer(),
+      // ############# Body #############
+      body: RefreshIndicator(
+        key: _playerListRefreshKey,
+        onRefresh: _loadPageData,
+        child: Consumer2<PlayerProvider, ExamProvider>(
+          builder: (context, playerProvider, examProvider, child) {
             // Wait until provider is ready
             if (examProvider.state == ProviderState.ready) {
-              Map<int, Player> list = {};
+              Map<int, Player> list = {}; // List of players to be displayed
+
+              // First it checks the filter state. If it's set to empty, add all players to the list.
+              // Otherwise, it starts the filtering process by getting every exam entry on the given date range
+              // It then iterates through every player, and depending on what filter state it's selected,
+              // tries to find any exam (or none) that contains the player id of the current player iteration
               if (_filter == _ExamFilters.empty) {
                 list = playerProvider.items;
               } else {
+                List<Exam> exams = examProvider.getByDate(_filterPeriod).values.toList();
                 for (var player in playerProvider.items.values) {
-                  List<Exam> exams =
-                      examProvider.getByDate(_filterPeriod).values.toList();
                   if (_filter == _ExamFilters.hasTests &&
                       exams.any((element) => element.player.id == player.id)) {
                     list.putIfAbsent(player.id!, () => player);
@@ -220,43 +223,47 @@ class _PlayerListViewState extends State<PlayerListView> {
                   }
                 }
               }
-              return ListView.builder(
+
+              if (list.isEmpty) {
+                return Center(
+                  child: Text(
+                    "Não foram encontrados nenhum jogadores.",
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                );
+              }
+              return ListView.separated(
                 itemCount: list.length,
                 itemBuilder: (context, index) {
                   Player player = list.values.elementAt(index);
-                  return Column(
-                    children: [
-                      ListTile(
-                          leading: FutureImage(
-                            image: player.picture!,
-                            errorImageUri:
-                                'assets/images/placeholder-player.png',
-                            aspectRatio: 1 / 1,
-                            borderRadius: BorderRadius.circular(100),
-                            color: Colors.white,
-                          ),
-                          title: Text(player.nickname ?? player.name),
-                          subtitle: Text(player.country.name),
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    PlayerView(player: player),
-                                maintainState: true,
-                              ),
-                            );
-                          }),
-                      const Divider()
-                    ],
+                  return ListTile(
+                    leading: FutureImage(
+                      image: player.picture!,
+                      errorImageUri: 'assets/images/placeholder-player.png',
+                      aspectRatio: 1 / 1,
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.white,
+                    ),
+                    title: Text(player.nickname ?? player.name),
+                    subtitle: Text(player.country.name),
+                    onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => PlayerView(player: player),
+                      ),
+                    ),
                   );
                 },
+                separatorBuilder: (context, index) => const Divider(),
               );
             } else {
+              // Fallback render if providers aren't ready
               return const Center(
                 child: CircularProgressIndicator(),
               );
             }
-          }),
-        ));
+          },
+        ),
+      ),
+    );
   }
 }
