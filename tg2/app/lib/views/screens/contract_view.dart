c871ec3b9824142c28bbd3a55045a73ff99dab48
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-26 12:49:59 +0000

    tg2 - update contract view

--- tg2/app/lib/views/screens/contract_view.dart
+++ tg2/app/lib/views/screens/contract_view.dart
@@ -11,7 +11,7 @@ class ContractView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("Contract/V: Building...");
+    print('Contract/V: Building...');
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
@@ -25,7 +25,6 @@ class ContractView extends StatelessWidget {
           ),
         ),
         Container(
-          width: double.infinity,
           padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
           decoration: BoxDecoration(
             borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
@@ -34,58 +33,69 @@ class ContractView extends StatelessWidget {
           child: Column(
             children: [
               Text(
-                "Contrato nº${contract.id}",
+                'Contrato ${contract.id}',
                 style: Theme.of(context).textTheme.titleLarge,
               ),
               const SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Flexible(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+              Card(
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(16))),
+                child: IntrinsicHeight(
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Contratante", style: Theme.of(context).textTheme.subtitle1),
-                        const SizedBox(height: 16),
-                        FutureImage(
-                          image: contract.club.picture!,
-                          errorImageUri: 'assets/images/placeholder-club.png',
-                          height: 64,
-                          aspectRatio: 1 / 1,
+                        Flexible(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Contratante', style: Theme.of(context).textTheme.subtitle1),
+                              const SizedBox(height: 16),
+                              FutureImage(
+                                image: contract.club.picture!,
+                                errorImageUri: 'assets/images/placeholder-club.png',
+                                height: 64,
+                                aspectRatio: 1 / 1,
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                contract.club.name,
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.headline6,
+                              ),
+                            ],
+                          ),
                         ),
-                        const SizedBox(height: 8),
-                        Text(
-                          contract.club.name,
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
+                        const SizedBox(width: 8),
+                        Flexible(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Contratado', style: Theme.of(context).textTheme.subtitle1),
+                              const SizedBox(height: 16),
+                              FutureImage(
+                                image: contract.player.picture!,
+                                errorImageUri: 'assets/images/placeholder-player.png',
+                                height: 64,
+                                aspectRatio: 1 / 1,
+                                borderRadius: BorderRadius.circular(100),
+                                color: Colors.white,
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                contract.player.name,
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.headline6,
+                              ),
+                            ],
+                          ),
+                        )
                       ],
                     ),
                   ),
-                  Flexible(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Contratado", style: Theme.of(context).textTheme.subtitle1),
-                        const SizedBox(height: 16),
-                        FutureImage(
-                          image: contract.player.picture!,
-                          errorImageUri: 'assets/images/placeholder-player.png',
-                          height: 64,
-                          aspectRatio: 1 / 1,
-                          borderRadius: BorderRadius.circular(100),
-                          color: Colors.white,
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          contract.player.name,
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                ),
               ),
               const Divider(),
               Row(
@@ -97,8 +107,8 @@ class ContractView extends StatelessWidget {
                         margin: const EdgeInsets.all(16),
                         child: Column(
                           children: [
-                            Text("Posição", style: Theme.of(context).textTheme.subtitle1),
-                            const SizedBox(height: 8),
+                            Text('Posição', style: Theme.of(context).textTheme.subtitle1),
+                            const Divider(height: 8),
                             Text(contract.position.name,
                                 style: Theme.of(context).textTheme.headline6),
                           ],
@@ -112,10 +122,15 @@ class ContractView extends StatelessWidget {
                         margin: const EdgeInsets.all(16),
                         child: Column(
                           children: [
-                            Text("Número", style: Theme.of(context).textTheme.subtitle1),
+                            Text(
+                              'Número',
+                              style: Theme.of(context).textTheme.subtitle1,
+                            ),
                             const SizedBox(height: 8),
-                            Text("${contract.number}",
-                                style: Theme.of(context).textTheme.headline6),
+                            Text(
+                              '${contract.number}',
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
                           ],
                         ),
                       ),
@@ -129,24 +144,31 @@ class ContractView extends StatelessWidget {
                   margin: const EdgeInsets.all(16),
                   child: Column(
                     children: [
-                      Text("Prazo", style: Theme.of(context).textTheme.subtitle1),
+                      Text(
+                        'Duração do Contrato',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
                       const SizedBox(height: 8),
                       Wrap(
                         crossAxisAlignment: WrapCrossAlignment.center,
                         spacing: 8,
                         children: [
-                          Text(DateUtilities().toYMD(contract.period.start),
-                              style: Theme.of(context).textTheme.headline6),
-                          Text("a", style: Theme.of(context).textTheme.subtitle1),
-                          Text(DateUtilities().toYMD(contract.period.end),
-                              style: Theme.of(context).textTheme.headline6),
+                          Text(
+                            DateUtilities().toYMD(contract.period.start),
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                          Text('a', style: Theme.of(context).textTheme.subtitle1),
+                          Text(
+                            DateUtilities().toYMD(contract.period.end),
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
                         ],
                       ),
                       const SizedBox(height: 8),
+                      // Displays contract state, if active, inactive or pending start
+                      // If active and close to expiration, display remaining days
                       Text(
-                          (contract.remainingTime.inDays > 0)
-                              ? "(${contract.remainingTime.inDays} dias restantes)"
-                              : "Expirado",
+                          'Estado: ${(contract.active) ? 'Ativo ${(contract.needsRenovation) ? '(${contract.remainingTime.inDays} dias restantes)' : ''}' : (contract.remainingTime.inDays < 0) ? 'Expirado' : 'Aguardando ínicio efetivo'}',
                           style: Theme.of(context).textTheme.caption),
                     ],
                   ),
