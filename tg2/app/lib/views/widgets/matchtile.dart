4025c05890b71de3bf3624fa6c157bd2e4a9d5be
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-27 12:56:29 +0000

    tg2 - update matchtile

--- tg2/app/lib/views/widgets/matchtile.dart
+++ tg2/app/lib/views/widgets/matchtile.dart
@@ -13,38 +13,65 @@ class MatchTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Column(
-          children: [
-            Text(
-                "${DateUtilities().toYMD(match.date)} ${DateUtilities().toHM(match.date)}",
-                style: Theme.of(context).textTheme.caption),
-            const SizedBox(height: 8),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              FutureImage(
-                image: match.clubHome.picture!,
-                errorImageUri: 'assets/images/placeholder-club.png',
-                height: 48,
-                aspectRatio: 1 / 1,
-              ),
-              Row(
-                children: [
-                  Text("${match.homeScore}",
-                      style: Theme.of(context).textTheme.titleLarge),
-                  const SizedBox(width: 16),
-                  Text(":", style: Theme.of(context).textTheme.subtitle1),
-                  const SizedBox(width: 16),
-                  Text("${match.awayScore}",
-                      style: Theme.of(context).textTheme.titleLarge),
-                ],
-              ),
-              FutureImage(
-                image: match.clubAway.picture!,
-                errorImageUri: 'assets/images/placeholder-club.png',
-                height: 48,
-                aspectRatio: 1 / 1,
-              ),
-            ]),
-          ],
+        title: Container(
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          child: Column(
+            children: [
+              Text("${DateUtilities().toYMD(match.date)} ${DateUtilities().toHM(match.date)}",
+                  style: Theme.of(context).textTheme.caption),
+              const SizedBox(height: 8),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      FutureImage(
+                        image: match.clubHome.picture!,
+                        errorImageUri: 'assets/images/placeholder-club.png',
+                        height: 48,
+                        aspectRatio: 1 / 1,
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        match.clubHome.name,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("${match.homeScore}", style: Theme.of(context).textTheme.headlineMedium),
+                      const SizedBox(width: 16),
+                      Text(":", style: Theme.of(context).textTheme.headlineSmall),
+                      const SizedBox(width: 16),
+                      Text("${match.awayScore}", style: Theme.of(context).textTheme.headlineMedium),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      FutureImage(
+                        image: match.clubAway.picture!,
+                        errorImageUri: 'assets/images/placeholder-club.png',
+                        height: 48,
+                        aspectRatio: 1 / 1,
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        match.clubAway.name,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
+            ],
+          ),
         ),
         onTap: () {
           showModalBottomSheet(
