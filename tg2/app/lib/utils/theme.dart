8415b15e04eee38e19a94ed6a42d2ba6d96edd6a
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-28 20:35:56 +0000

    tg2 - update theme

--- tg2/app/lib/utils/theme.dart
+++ tg2/app/lib/utils/theme.dart
@@ -2,38 +2,40 @@ import 'package:flutter/material.dart';
 
 // Theme file for the app
 class AppTheme {
-  static ColorScheme colorScheme = const ColorScheme(
+  static ColorScheme colorScheme = ColorScheme.fromSeed(seedColor: Color(0xFF001941)).copyWith(
     brightness: Brightness.light,
-    primary: Color(0xFF2C3965),
+    primary: Color(0xFF22305E),
     onPrimary: Colors.white,
-    primaryContainer: Color(0xFF7A839E),
-    onPrimaryContainer: Colors.black,
-    secondary: Color(0xFF151F41),
+    primaryContainer: Color(0xFF4F597E),
+    onPrimaryContainer: Colors.white,
+    secondary: Color(0xFF2258A5),
     onSecondary: Colors.white,
-    secondaryContainer: Color(0xFF22305E),
-    onSecondaryContainer: Colors.white,
-    tertiary: Color(0xFF2258A5),
+    //secondaryContainer: Color(0xFF22305E),
+    //onSecondaryContainer: Colors.white,
+    tertiary: Color(0xFF000925),
     onTertiary: Colors.white,
-    tertiaryContainer: Color(0xFFD7E2FF),
-    onTertiaryContainer: Colors.black,
+    tertiaryContainer: Color(0xFF151F41),
+    onTertiaryContainer: Colors.white,
     error: Color(0xFFE57373),
     onError: Colors.white,
-    errorContainer: Color(0xFFFFDAD8),
-    onErrorContainer: Colors.black,
+    //errorContainer: Color(0xFFFFDAD8),
+    //onErrorContainer: Colors.black,
     background: Color(0xFFF5F5F5),
     onBackground: Colors.black,
     surface: Colors.white,
     onSurface: Colors.black,
     surfaceVariant: Color(0xFFE8EAEE),
     onSurfaceVariant: Colors.black,
-    inverseSurface: Color(0xFF000925),
-    onInverseSurface: Colors.white,
-    inversePrimary: Color(0xFF4F597E),
   );
 
   static ThemeData theme = ThemeData(
     colorScheme: colorScheme,
-    appBarTheme: AppBarTheme(elevation: 0, backgroundColor: colorScheme.inverseSurface),
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      centerTitle: true,
+      backgroundColor: colorScheme.tertiary,
+    ),
     cardTheme: const CardTheme(elevation: 0),
+    splashColor: Colors.transparent,
   );
 }
