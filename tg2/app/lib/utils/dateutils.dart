2653e1ce93a99e9742c1da0b5218a4c58e64fead
Miguel Leirosa <miguelleirosa@gmail.com>
2023-01-30 20:57:25 +0000

    tg2 - remove dateutils toString methods

--- tg2/app/lib/utils/dateutils.dart
+++ tg2/app/lib/utils/dateutils.dart
@@ -7,29 +7,12 @@ class DateUtilities {
     var splitted = json.split(",");
     String start = splitted[0].replaceAll("[", "");
     String end = splitted[1].replaceAll(")", "");
-    return DateTimeRange(
-        start: DateTime.parse(start), end: DateTime.parse(end));
+    return DateTimeRange(start: DateTime.parse(start), end: DateTime.parse(end));
   }
 
   // Encodes DateTimeRange into Postgres compatible date format
   String encoder(DateTimeRange date) {
     return "[${date.start.year}-${date.start.month}-${date.start.day},"
         "${date.end.year}-${date.end.month}-${date.end.day})";
-  }
-
-  // Turns date into YYYY-MM-DD format string
-  String toYMD(DateTime date) {
-    String year = date.year.toString();
-    String month = ((date.month < 10) ? "0" : "") + date.month.toString();
-    String day = ((date.day < 10) ? "0" : "") + date.day.toString();
-    return "$year-$month-$day";
-  }
-
-  // Turns date into HH:MM format string
-  String toHM(DateTime date) {
-    String hour = ((date.hour < 10) ? "0" : "") + date.hour.toString();
-    String minute = ((date.minute < 10) ? "0" : "") + date.minute.toString();
-
-    return "$hour:$minute";
   }
 }
