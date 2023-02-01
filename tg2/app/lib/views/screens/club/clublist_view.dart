698a3c24b6f34924e42f9d2768c8cebf11013c1c
Miguel Leirosa <miguelleirosa@gmail.com>
2023-02-01 19:03:11 +0000

    tg2 - update club list view

--- tg2/app/lib/views/screens/club/clublist_view.dart
+++ tg2/app/lib/views/screens/club/clublist_view.dart
@@ -19,22 +19,21 @@ class ClubListView extends StatefulWidget {
 }
 
 class _ClubListViewState extends State<ClubListView> {
-  List<Map<String, dynamic>> _data = []; // Data structure of the page
-  List<int> matchweeks = [];
-
-  final GlobalKey<RefreshIndicatorState> _clubListRefreshKey = GlobalKey<RefreshIndicatorState>();
+  List<Map<String, dynamic>> data = []; // Data structure of the page
+  List<int> filterData = []; // Filter data (Dropdown items)
   int? selectedMatchweek;
 
-  void _setMatchweek({int? matchweek}) {
+  void _setMatchweek(int? matchweek) {
     setState(() => selectedMatchweek = matchweek);
-    _data.clear();
+    data.clear();
   }
 
   // Reload providers used by the page, displays snackbar if exception occurs
   Future _loadPageData() async {
     try {
       await Provider.of<ClubProvider>(context, listen: false).get();
-      _data.clear();
+      filterData.clear();
+      data.clear();
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
@@ -53,6 +52,198 @@ class _ClubListViewState extends State<ClubListView> {
     WidgetsBinding.instance.addPostFrameCallback((context) => _loadPageData());
   }
 
+  @override
+  Widget build(BuildContext context) {
+    print("ClubList/V: Building...");
+    return Consumer2<ClubProvider, MatchProvider>(builder: (context, clubProvider, matchProvider, child) {
+      // Since match provider depends on club provider, its state dictates the page's state
+      // But the page data does not depend on match data being empty or not
+      if (matchProvider.state != ProviderState.busy && data.isEmpty) {
+        filterData = matchProvider.getMatchweeks();
+        // Query data from clubs and matches to sort them by who has the most points and matches
+        data = clubProvider.items.values
+            .where((club) => club.playing == true)
+            .map((club) => {
+                  'club': club,
+                  'matches': matchProvider.getByClub(club).length,
+                  'points': matchProvider.getClubPoints(club: club, matchweek: selectedMatchweek),
+                })
+            .toList();
+        data
+          ..sort((b, a) => a['matches'].compareTo(b['matches']))
+          ..sort((b, a) => a['points'].compareTo(b['points']));
+      }
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Clubes"),
+          actions: [
+            // ############# Filter Popup #############
+            filterData.isNotEmpty
+                ? IconButton(
+                    icon: const Icon(Icons.sort_outlined),
+                    tooltip: 'Filtos',
+                    onPressed: () => {
+                      showDialog<void>(
+                        context: context,
+                        builder: (context) {
+                          return StatefulBuilder(
+                            builder: (BuildContext context, StateSetter setState) => Dialog(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16),
+                                child: HeaderWidget(
+                                  headerText: 'Filtros',
+                                  headerAction: IconButton(
+                                    splashRadius: 24,
+                                    icon: Icon(Icons.clear),
+                                    onPressed: () => Navigator.pop(context),
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Expanded(
+                                            flex: 2,
+                                            child: DropdownButtonFormField<int>(
+                                              items: filterData.map<DropdownMenuItem<int>>((int value) {
+                                                return DropdownMenuItem<int>(
+                                                  value: value,
+                                                  child: Text('Jornada $value'),
+                                                );
+                                              }).toList(),
+                                              value: selectedMatchweek,
+                                              onChanged: (value) {
+                                                setState(() {
+                                                  _setMatchweek(value);
+                                                });
+                                              },
+                                              decoration: const InputDecoration(
+                                                border: OutlineInputBorder(),
+                                                labelText: 'Jornada',
+                                              ),
+                                              isExpanded: true,
+                                            ),
+                                          ),
+                                          IconButton(
+                                            onPressed: () => setState(() {
+                                              _setMatchweek(null);
+                                            }),
+                                            icon: Icon(Icons.clear),
+                                          ),
+                                          Expanded(child: SizedBox())
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      )
+                    },
+                  )
+                : Container()
+          ],
+        ),
+        drawer: const MenuDrawer(),
+        body: RefreshIndicator(
+          key: GlobalKey<RefreshIndicatorState>(),
+          onRefresh: _loadPageData,
+          child: Builder(
+            builder: (BuildContext context) {
+              if (data.isNotEmpty) {
+                return Column(children: [
+                  Material(
+                    color: Theme.of(context).colorScheme.surfaceVariant,
+                    child: ListTile(
+                      dense: true,
+                      title: Row(
+                        children: [
+                          Expanded(child: Text("Nome do Clube", style: Theme.of(context).textTheme.labelMedium)),
+                          Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 4),
+                              alignment: Alignment.center,
+                              width: 48,
+                              child: Text("Jogos", style: Theme.of(context).textTheme.labelMedium)),
+                          Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 4),
+                              alignment: Alignment.center,
+                              width: 48,
+                              child: Text("Pontos", style: Theme.of(context).textTheme.labelMedium)),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Divider(height: 0, thickness: 1),
+                  Expanded(
+                    child: ListView.separated(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        Club club = data[index]['club'];
+                        int totalMatches = data[index]['matches'];
+                        int totalPoints = data[index]['points'];
+                        return ListTile(
+                          tileColor: Theme.of(context).colorScheme.surface,
+                          dense: true,
+                          leading: FutureImage(
+                            image: club.picture!,
+                            errorImageUri: 'assets/images/placeholder-club.png',
+                            aspectRatio: 1 / 1,
+                            height: 42,
+                          ),
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(child: Text(club.name, style: Theme.of(context).textTheme.titleSmall)),
+                              Container(
+                                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                                  alignment: Alignment.center,
+                                  width: 48,
+                                  child: Text('$totalMatches', style: Theme.of(context).textTheme.labelLarge)),
+                              Container(
+                                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                                  alignment: Alignment.center,
+                                  width: 48,
+                                  child: Text('$totalPoints', style: Theme.of(context).textTheme.labelLarge)),
+                            ],
+                          ),
+                          subtitle: Text(club.stadium!.city, style: Theme.of(context).textTheme.caption),
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    ClubView(club: club, selectedMatchweek: selectedMatchweek),
+                                maintainState: false,
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      separatorBuilder: (context, index) => const Divider(height: 0),
+                    ),
+                  )
+                ]);
+              }
+              if (data.isEmpty && matchProvider.state == ProviderState.busy) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              // If nothing is found
+              return Center(
+                child: Wrap(direction: Axis.vertical, crossAxisAlignment: WrapCrossAlignment.center, children: [
+                  const Text('Não foram encontrados nenhuns clubes'),
+                  ElevatedButton(onPressed: _loadPageData, child: const Text('Tentar novamente')),
+                ]),
+              );
+            },
+          ),
+        ),
+      );
+    });
+  }
+/*
   @override
   Widget build(BuildContext context) {
     print("ClubList/V: Building...");
@@ -137,7 +328,7 @@ class _ClubListViewState extends State<ClubListView> {
             // Since match provider depends on club provider, its state dictates the page's state
             // But the page data does not depend on match data being empty or not
             if (matchProvider.state != ProviderState.busy && _data.isEmpty) {
-              matchweeks = matchProvider.getMatchweeks();
+              _setMatchweeks(matchProvider.getMatchweeks());
               // Query data from clubs and matches to sort them by who has the most points and matches
               _data = clubProvider.items.values
                   .where((club) => club.playing == true)
@@ -240,4 +431,6 @@ class _ClubListViewState extends State<ClubListView> {
       ),
     );
   }
+
+   */
 }
