55830bdd106dce8166ebb6b4718f1be4509d3169
Miguel Leirosa <miguelleirosa@gmail.com>
2023-02-01 19:43:38 +0000

    tg2 - update match tile

--- tg2/app/lib/views/widgets/matchtile.dart
+++ tg2/app/lib/views/widgets/matchtile.dart
@@ -14,77 +14,152 @@ class MatchTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        tileColor: Theme.of(context).colorScheme.surface,
-        title: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-          child: Column(
-            children: [
-              Text(
-                  showTimeOnly
-                      ? DateFormat.Hm('pt_PT').format(match.date)
-                      : DateFormat.yMMMMd('pt_PT').add_Hm().format(match.date),
-                  style: Theme.of(context).textTheme.bodyMedium),
-              const SizedBox(height: 16),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      tileColor: Theme.of(context).colorScheme.surface,
+      title: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Column(
+          children: [
+            // TIMESTAMP
+            Text(
+              showTimeOnly
+                  ? DateFormat.Hm('pt_PT').format(match.date)
+                  : DateFormat.yMMMMd('pt_PT').add_Hm().format(match.date),
+              style: Theme.of(context).textTheme.caption,
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                // HOME CLUB BADGE
                 Expanded(
-                  child: Column(
-                    children: [
-                      FutureImage(
-                        image: match.clubHome.picture!,
-                        errorImageUri: 'assets/images/placeholder-club.png',
-                        height: 48,
-                        aspectRatio: 1 / 1,
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        match.clubHome.name,
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.caption,
-                      ),
-                    ],
+                  child: FutureImage(
+                    image: match.clubHome.picture!,
+                    errorImageUri: 'assets/images/placeholder-club.png',
+                    height: 48,
+                    aspectRatio: 1 / 1,
                   ),
                 ),
+                // SCORE
                 Expanded(
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Text('${match.homeScore}', style: Theme.of(context).textTheme.headlineMedium),
-                      const SizedBox(width: 16),
                       Text(':', style: Theme.of(context).textTheme.headlineSmall),
-                      const SizedBox(width: 16),
                       Text('${match.awayScore}', style: Theme.of(context).textTheme.headlineMedium),
                     ],
                   ),
                 ),
+                // AWAY CLUB BADGE
                 Expanded(
-                  child: Column(
-                    children: [
-                      FutureImage(
-                        image: match.clubAway.picture!,
-                        errorImageUri: 'assets/images/placeholder-club.png',
-                        height: 48,
-                        aspectRatio: 1 / 1,
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        match.clubAway.name,
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.caption,
-                      ),
-                    ],
+                  child: FutureImage(
+                    image: match.clubAway.picture!,
+                    errorImageUri: 'assets/images/placeholder-club.png',
+                    height: 48,
+                    aspectRatio: 1 / 1,
                   ),
                 ),
-              ]),
-            ],
-          ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            // CLUBS NAME
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    match.clubHome.name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ),
+                const Spacer(),
+                Expanded(
+                  child: Text(
+                    match.clubAway.name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-        onTap: () {
-          showModalBottomSheet(
-              isScrollControlled: true,
-              isDismissible: true,
-              backgroundColor: Colors.transparent,
-              context: context,
-              builder: (context) => MatchView(match: match));
-        });
+      ),
+    );
+
+    return ListTile(
+      tileColor: Theme.of(context).colorScheme.surface,
+      title: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        child: Column(
+          children: [
+            Text(
+                showTimeOnly
+                    ? DateFormat.Hm('pt_PT').format(match.date)
+                    : DateFormat.yMMMMd('pt_PT').add_Hm().format(match.date),
+                style: Theme.of(context).textTheme.bodyMedium),
+            const SizedBox(height: 16),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    FutureImage(
+                      image: match.clubHome.picture!,
+                      errorImageUri: 'assets/images/placeholder-club.png',
+                      height: 48,
+                      aspectRatio: 1 / 1,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      match.clubHome.name,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('${match.homeScore}', style: Theme.of(context).textTheme.headlineMedium),
+                    const SizedBox(width: 16),
+                    Text(':', style: Theme.of(context).textTheme.headlineSmall),
+                    const SizedBox(width: 16),
+                    Text('${match.awayScore}', style: Theme.of(context).textTheme.headlineMedium),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    FutureImage(
+                      image: match.clubAway.picture!,
+                      errorImageUri: 'assets/images/placeholder-club.png',
+                      height: 48,
+                      aspectRatio: 1 / 1,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      match.clubAway.name,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ],
+        ),
+      ),
+      onTap: () {
+        showModalBottomSheet(
+          isScrollControlled: true,
+          isDismissible: true,
+          backgroundColor: Colors.transparent,
+          context: context,
+          builder: (context) => MatchView(match: match),
+        );
+      },
+    );
   }
 }
