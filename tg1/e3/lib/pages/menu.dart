7a0d383fe869e6ce55ccebb853e829fb780b3a53
Miguel Leirosa <miguelleirosa@gmail.com>
2023-04-28 13:12:58 +0100

    tg1:e3 - fixed recursion to prevent stack overflow

--- tg1/e3/lib/pages/menu.dart
+++ tg1/e3/lib/pages/menu.dart
@@ -6,25 +6,29 @@ import 'package:e3/pages/reports.dart';
 import 'dashboard.dart';
 
 void menu(){
-  dashboard();
-  stdout.writeln("MENU");
-  stdout.writeln("\t1. Dados");
-  if(Policy.cache.isNotEmpty) stdout.writeln("\t2. Relatórios");
-  stdout.write("Insira o digito da opção desejada: ");
-  try {
-    String? option = stdin.readLineSync();
-    switch(option) {
-      case '1':
-        manager();
-        break;
-      case '2':
-        if(Policy.cache.isNotEmpty) reports();
-        break;
-      default:
-        stdout.writeln("Opção inválida!");
+  while(true) {
+    dashboard();
+    stdout.writeln("MENU");
+    stdout.writeln("\t1. Dados");
+    if(Policy.cache.isNotEmpty) stdout.writeln("\t2. Relatórios");
+    stdout.writeln("\t3. Sair");
+    stdout.write("Insira o digito da opção desejada: ");
+    try {
+      String? option = stdin.readLineSync();
+      switch(option) {
+        case '1':
+          manager();
+          break;
+        case '2':
+          if(Policy.cache.isNotEmpty) reports();
+          break;
+        case '3':
+          return;
+        default:
+          stdout.writeln("Opção inválida!");
+      }
+    } catch (e) {
+      stdout.writeln("Opção inválida!");
     }
-  } catch (e) {
-    stdout.writeln("Opção inválida!");
-  }
-  menu(); // Recursion until correct option
+  } // Recursion until correct option
 }
